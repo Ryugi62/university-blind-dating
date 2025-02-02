ced206153b684755198ca5a678c3d6917bf555c5
Ryugi62 <xorjf1027@naver.com>
2025-02-02 17:15:18 +0900

    refactor ( backend ) : userRoutes.js에 getUser와 registerUser 호출 순서에 대한 주석 추가

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,4 +6,10 @@ const router = Router();
 router.post("/register", registerUser);
 router.get("/:email", getUser);
 
+// getUser한 다음에 registerUser해야되면
+// router.get("/test", async (req, res, next) => {
+//     await getUser(req, res, next);
+//     await registerUser(req, res, next);
+// });
+
 export default router;
